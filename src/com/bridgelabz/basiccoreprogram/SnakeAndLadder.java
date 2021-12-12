9dafd4f6820651122d5e57586c19ebd5ec80b6ae
vaibs <vaibhavmuratkar2@gmail.com>
2021-12-12 16:25:14 +0530

    Roll Dice-UC2

--- src/com/bridgelabz/basiccoreprogram/SnakeAndLadder.java
+++ src/com/bridgelabz/basiccoreprogram/SnakeAndLadder.java
@@ -1,9 +1,16 @@
 package com.bridgelabz.basiccoreprogram;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println(" Welcome to the Game ");
         int initial_pos = 0;
         System.out.println(" Position of the Player: " + initial_pos);
+
+        // Generate Random Num from 1 to 6
+        Random random = new Random();
+        int dice = random.nextInt(6) + 1;
+        System.out.println(" Rolled Num: " + dice);
     }
 }
