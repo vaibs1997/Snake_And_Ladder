5fc66cf58769d6f96a5837c850a4144424212223
vaibs <vaibhavmuratkar2@gmail.com>
2021-12-12 16:13:05 +0530

    Initial_Position-UC1

--- src/com/bridgelabz/basiccoreprogram/SnakeAndLadder.java
+++ src/com/bridgelabz/basiccoreprogram/SnakeAndLadder.java
@@ -2,6 +2,8 @@ package com.bridgelabz.basiccoreprogram;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        
+        System.out.println(" Welcome to the Game ");
+        int initial_pos = 0;
+        System.out.println(" Position of the Player: " + initial_pos);
     }
 }
